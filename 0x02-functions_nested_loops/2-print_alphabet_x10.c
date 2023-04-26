0e4d2c64d85552e6e96b81fd810584af2db7ce24
Lesedichar17 <lesedicharlotte22@gmail.com>
2023-04-26 14:18:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the al[habet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; 10; i++)
+	{
+		for (j = 97; <= 122; j++)
+		{
+			_[utchar(j);
+		}
+		_putchar('\n');
+	}
+}
