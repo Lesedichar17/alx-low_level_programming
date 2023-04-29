be46ba223bfb77feb5950b27bbbc43dd891cdfd3
Lesedichar17 <lesedicharlotte22@gmail.com>
2023-04-29 15:15:17 -0700

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 include "main.h"
 
 /**
- * print_line draws a straight line in the terminal
+ * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
 void print_line(int n)
@@ -9,7 +9,7 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else 
+	} else	
 	{
 		int i;
 
